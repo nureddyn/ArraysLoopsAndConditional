20f76d1f7d7fd483e203b1ff50dc220895958ce4
nureddyn <nureddynballout@gmail.com>
2023-10-26 18:39:44 -0300

    Excited Kittens answered

--- answers.js
+++ answers.js
@@ -132,16 +132,36 @@ You should be modifying the elements by accessing them. It is up to you which me
 /////////////////////////////////
 
 // With the following multi-dimensional array
-const whereIsWaldo = [
-        ["Timmy", "Frank"], "Eggbert",
-        ["Lucinda", "Jacc", "Neff", "Snoop"],
-        ["Petunia", ["Baked Goods", "Waldo"]]
-];
-// Remove Eggbert (hint look at the slice/splice method(s))
-whereIsWaldo.splice(1, 1);
+// const whereIsWaldo = [
+//         ["Timmy", "Frank"], "Eggbert",
+//         ["Lucinda", "Jacc", "Neff", "Snoop"],
+//         ["Petunia", ["Baked Goods", "Waldo"]]
+// ];
+// // Remove Eggbert (hint look at the slice/splice method(s))
+// whereIsWaldo.splice(1, 1);
 
-// Change "Neff" to "No One"
-whereIsWaldo[1][2] = "No one";
+// // Change "Neff" to "No One"
+// whereIsWaldo[1][2] = "No one";
 
-// Access and console.log "Waldo"
-console.log(whereIsWaldo[2][1][1]);
+// // Access and console.log "Waldo"
+// console.log(whereIsWaldo[2][1][1]);
+
+/////////////////////////////////
+// Excited Kitten
+/////////////////////////////////
+
+// 1. Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+/* 2. For every even number in your loop, log "...human...why you taking pictures of me?...",
+"...the catnip made me do it...", or "...why does the red dot always get away..." at random.*/
+let sentences = [
+    "...human...why you taking pictures of me?...",
+    "...the catnip made me do it...",
+    "...why does the red dot always get away..."
+]
+for (let times = 0; times < 20; times++) {
+    console.log("Love me, pet me! HSSSSSS!");
+    if (times % 2 === 0) {
+        let randomIndex = Math.floor(Math.random() * sentences.length);
+        console.log(sentences[randomIndex]);
+    }
+}
